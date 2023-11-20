c7860baf00848b29ed1fafd64b2db240b84546a0
Okwudili-Daniel <144442872+Okwudili-Daniel@users.noreply.github.com>
2023-11-20 10:55:55 +0100

    index

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import express, { Application,Request,Response } from 'express';
+import cors from 'cors';
 
-const port: number = 3344
 const app: Application = express();
+const port: number = 3344
+
+app.use(express.json())
+app.use(cors());
 
 const server = app.listen(port, () =>{
     console.log("server listening on port");
